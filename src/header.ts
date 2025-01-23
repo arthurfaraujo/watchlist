e391e1da7557ecd15559ac51632157a8f3a37f2e
davicesarmorais <davicesarmorais@gmail.com>
2025-01-23 17:14:58 -0300

    change navbar look

--- src/header.ts
+++ src/header.ts
@@ -2,15 +2,15 @@ import SearchBar from "./searchBar"
 
 export default function Header() {
     return `
-<header class="t-1 sticky text-neutral-100">
-  <nav class="flex justify-between items-center p-4">
+<nav class="bg-neutral-800 w-full">
+  <div class="w-4/6 mx-auto text-neutral-100 flex justify-between items-center p-2 ">
     <p class="font-bold text-2xl cursor-pointer">WatchList</p>
     ${SearchBar()}
     <ul class="font-semibold flex justify-between items-center gap-8">
         <li class="cursor-pointer">Minhas listas</li>
         <li class="w-10 h-10 border rounded-full cursor-pointer"></liv>
     </ul>
-  </nav>
-</header> 
+  </div>
+</nav>
     `
 } 
