dd04e92750382e5823b3839c3cf42b9c44720ed6
Davi Leite Alencar <davilt.alencar@gmail.com>
2025-01-25 11:51:39 -0300

    ♻️refactor: tentaiva de melhorar a exibição do carrosel

--- src/homepage/components/moviesList.ts
+++ src/homepage/components/moviesList.ts
@@ -35,15 +35,16 @@ async function createCarousel(titulo: string, apiFunction: () => Promise<any>) {
         const response = await apiFunction.call(apiService);
         response.results.forEach((movie: any) => {
             const movieCard = document.createElement("div");
-            movieCard.className = "flex-shrink-0 w-64 bg-neutral-700 text-neutral-100 p-2 rounded-md cursor-pointer flex flex-col justify-between";
+            movieCard.className = "flex-shrink-0 w-64 bg-neutral-700 text-neutral-100 p-2 rounded-md cursor-pointer flex flex-col justify-between w-1/5";
             movieCard.innerHTML = `
                 <div class="h-96 overflow-hidden">
-                    <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="${movie.title}" class="w-full h-80 object-cover rounded-md">
+                    <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="${movie.title}" class="w-full h-80 object-cover rounded-md" id="movie-img">
                     <h2 class="text-xl font-bold mt-2 text-center">${movie.title}</h2>
                 </div>
                 <button class="mt-2 bg-blue-700 text-neutral-100 p-2 rounded-md w-full">Listar</button>
             `;
-            movieCard.addEventListener("click", () => MovieCard(movie.id));
+            const movieImg = movieCard.querySelector("#movie-img") as HTMLImageElement;
+            movieImg.addEventListener("click", () => MovieCard(movie.id));
             moviesContainer.appendChild(movieCard);
         });
     } catch (error) {
@@ -55,7 +56,7 @@ async function createCarousel(titulo: string, apiFunction: () => Promise<any>) {
 
 export async function moviesList() {
     const container = document.createElement("div");
-    container.className = "w-full flex flex-col items-center";
+    container.className = "w-4/5 flex flex-col items-center";
 
     container.appendChild(await createCarousel("Filmes em alta", ApiService.prototype.getTrendingAll));
 
