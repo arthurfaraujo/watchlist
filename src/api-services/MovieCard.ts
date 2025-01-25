0e5c24ab5086793e38d533ea215b1313296de356
Arthur Araújo <arthurfelipe5567@gmail.com>
2025-01-25 20:55:54 -0300

    feat: add watchlist

--- src/api-services/MovieCard.ts
+++ src/api-services/MovieCard.ts
@@ -9,7 +9,7 @@ export default async function MovieCard(movieId: string) {
         movieCard.innerHTML = `
             <div class="bg-neutral-800 text-neutral-100 p-4 rounded-md w-1/4">
                 <button class="text-neutral-100 float-right" id="closeButton">X</button>
-                <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="${movie.title}" class="w-full h-96 w-96 rounded-md">
+                <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="${movie.title}" class="w-full h-96 rounded-md">
                 <h2 class="text-2xl font-bold mt-2">${movie.title}</h2>
                 <p class="mt-1">${movie.overview}</p>
                 <p class="mt-1">Release Date: ${movie.release_date}</p>
--- src/components/header.ts
+++ src/components/header.ts
@@ -4,11 +4,11 @@ export default function Header() {
     return `
 <nav class="bg-neutral-800 w-full">
   <div class="w-4/6 mx-auto text-neutral-100 flex justify-between items-center p-2 ">
-    <p class="font-bold text-2xl cursor-pointer">WatchList</p>
+    <a href="/" class="text-xl font-bold">WatchList</a>
     ${SearchBar()}
     <ul class="font-semibold flex justify-between items-center gap-8">
-        <li class="cursor-pointer">Minhas listas</li>
-        <li class="w-10 h-10 border rounded-full cursor-pointer"></liv>
+      <li class="cursor-pointer"><a href='/mywatchlists'>Minhas Listas</a></li>
+      <li class="w-10 h-10 border rounded-full cursor-pointer"></liv>
     </ul>
   </div>
 </nav>
