6d6492e93e838a54af194a8187e5c75b89ec6280
davicesarmorais <davicesarmorais@gmail.com>
2025-01-27 16:07:16 -0300

    feat: get api response with search bar

--- src/components/header.ts
+++ src/components/header.ts
@@ -1,21 +1,22 @@
 import SearchBar from "./searchBar"
 
 export default function Header() {
-    return `
-<nav class="bg-neutral-800 w-full">
+  const header = document.createElement("nav");
+  header.className = "bg-neutral-800 w-full"
+  header.innerHTML = `
   <div class="max-w-screen-xl xl:mx-auto mx-4 text-neutral-100 flex justify-between items-center p-2 ">
     <a class="hidden sm:flex gap-2 font-bold text-xl cursor-pointer" href="/">
       <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="m20.84 2.18l-3.93.78l2.74 3.54l1.97-.4zm-6.87 1.36L12 3.93l2.75 3.53l1.96-.39zm-4.9.96l-1.97.41l2.75 3.53l1.96-.39zm-4.91 1l-.98.19a1.995 1.995 0 0 0-1.57 2.35L2 10l4.9-.97zM20 12v8H4v-8zm2-2H2v10a2 2 0 0 0 2 2h16c1.11 0 2-.89 2-2z"/></svg>
       <span>WatchList<span>
     </a>
-    ${SearchBar()}
+    <div class="w-1/2 sm:mx-4 mx-2" id="searchBarContainer"></div>
     <ul class="text-sm font-semibold flex justify-between items-center">
         <li class="cursor-pointer whitespace-nowrap"><a href='/mywatchlists'>Minhas Listas</a></li>
         <li class="flex justify-center items-center ml-4 w-10 h-10 border rounded-full cursor-pointer">
           <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><g fill="none" stroke="currentColor" stroke-dasharray="28" stroke-dashoffset="28" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"><path d="M4 21v-1c0 -3.31 2.69 -6 6 -6h4c3.31 0 6 2.69 6 6v1"><animate fill="freeze" attributeName="stroke-dashoffset" dur="0.4s" values="28;0"/></path><path d="M12 11c-2.21 0 -4 -1.79 -4 -4c0 -2.21 1.79 -4 4 -4c2.21 0 4 1.79 4 4c0 2.21 -1.79 4 -4 4Z"><animate fill="freeze" attributeName="stroke-dashoffset" begin="0.4s" dur="0.4s" values="28;0"/></path></g></svg>
         </liv>
     </ul>
-  </div>
-</nav>
-    `;
+  </div>`;
+  header.querySelector("#searchBarContainer")!.appendChild(SearchBar())
+  return header
 } 
--- src/components/searchBar.ts
+++ src/components/searchBar.ts
@@ -1,18 +1,71 @@
+import ApiService from "@/api-services/apiService";
+import { MediaType } from "@/api-services/MovieCard";
+
+interface Result {
+    id: number;
+    title: string;
+    name?: string;
+    media_type: MediaType;  // Assumindo que 'movie' ou 'tv' são os tipos possíveis
+    poster_path?: string;
+    release_date?: string;
+    vote_average?: number;
+}
+
+// Definindo um tipo para a resposta da API
+interface ApiResponse {
+    results: Result[];
+}
+
 export default function SearchBar() {
-    return `
-<form class="w-1/2 sm:mx-4 mx-2">
+    const apiService = new ApiService()
+    const searchBar = document.createElement("form")
+    searchBar.className = "w-full"
+    searchBar.innerHTML = `
     <label for="default-search" class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
     <div class="relative">
         <input type="search" id="default-search" class="block w-full p-2 pr-8 text-xs rounded-lg bg-neutral-700 border border-neutral-700 placeholder-neutral-400 text-white outline-none focus:border-neutral-600" placeholder="Pesquise por filmes/series..." required />
         
-        <div class="absolute inset-y-0 end-0 flex items-center pe-3 cursor-pointer">
+        <div id="lupa" class="absolute inset-y-0 end-0 flex items-center pe-3 cursor-pointer">
             <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
             </svg>
         </div>
 
         <button type="submit" class="hidden"></button>
-    </div>
-</form>
-`
+    </div>`;
+    searchBar.querySelector("#lupa")!.addEventListener("click", () => {
+        const input = searchBar.querySelector("input")!
+        if (input && input.checkValidity()) {
+            searchBar.submit();
+        } else {
+            input.reportValidity();     
+        }
+    })
+    
+    function showSearchResults(apiResponse: ApiResponse){
+        const searchBarContainer = document.getElementById("searchBarContainer")!
+        const content = document.createElement("div")
+        content.className = "";
+        content.innerHTML = apiResponse.results.map(item => {
+            return `
+            <div class="flex">
+                <p>${item.media_type === MediaType.filme? item.title : item.name}</p>
+            </div>`;
+        }).join("")
+        searchBarContainer.appendChild(content)
+    }
+
+    const input = searchBar.querySelector("input")!
+    let timeout;
+    input.addEventListener("keypress", () => {
+        clearTimeout(timeout!);
+        const delayTime = input.value.length > 4 ? 500 : 1500;
+        timeout = setTimeout(() => {
+            apiService.searchMulti(input.value).then((apiResponse) => {
+                showSearchResults(apiResponse)
+            })
+        }, delayTime)
+    })
+    
+    return searchBar
 }
