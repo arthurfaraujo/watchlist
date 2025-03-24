b617f0a2eb95fee3d18a9d67a3d885922ecfe6c7
Davi César <davicesarmorais@gmail.com>
2025-03-23 22:11:49 -0300

    refactor: remove fragments (<> and </>)

--- src/components/MovieCard.tsx
+++ src/components/MovieCard.tsx
@@ -19,44 +19,42 @@ export default function MovieCard({
     : "/placeholder.png";
 
   return (
-    <>
-      <div className="flex flex-col">
-        <div
-          onClick={() => openModal(media)}
-          className="cursor-pointer relative w-56 h-80">
-          <Image
-            id="movie-img"
-            src={imagePath}
-            alt={name}
-            width={500}
-            height={500}
-            className="cursor-pointer rounded-t w-full h-full object-cover"
-          />
-          <div className="absolute bottom-0 left-0 right-0 p-2 bg-black bg-opacity-70 text-white">
-            <h3 className="text-sm font-bold">{name}</h3>
-            <div className="text-xs flex items-center justify-between">
-              <p>
-                {new Date(
-                  media.release_date || media.first_air_date + "T00:00:00"
-                ).toLocaleDateString()}
+    <div className="flex flex-col">
+      <div
+        onClick={() => openModal(media)}
+        className="cursor-pointer relative w-56 h-80">
+        <Image
+          id="movie-img"
+          src={imagePath}
+          alt={name}
+          width={500}
+          height={500}
+          className="cursor-pointer rounded-t w-full h-full object-cover"
+        />
+        <div className="absolute bottom-0 left-0 right-0 p-2 bg-black bg-opacity-70 text-white">
+          <h3 className="text-sm font-bold">{name}</h3>
+          <div className="text-xs flex items-center justify-between">
+            <p>
+              {new Date(
+                media.release_date || media.first_air_date + "T00:00:00"
+              ).toLocaleDateString()}
+            </p>
+            {media.vote_average! > 0 && (
+              <p className="flex gap-1 items-center">
+                <MdStar className="text-yellow-400 text-sm" />
+                <span>{Number(media.vote_average).toFixed(1)}</span>
               </p>
-              {media.vote_average! > 0 && (
-                <p className="flex gap-1 items-center">
-                  <MdStar className="text-yellow-400 text-sm" />
-                  <span>{Number(media.vote_average).toFixed(1)}</span>
-                </p>
-              )}
-            </div>
+            )}
           </div>
         </div>
-        <button
-          onClick={() => setWatchlistMarket(!watchlistMarked)}
-          className={`${
-            watchlistMarked ? "bg-green-600" : "bg-blue-700"
-          } text-neutral-100 text-sm p-2 rounded-b w-full`}>
-          Watchlist +
-        </button>
       </div>
-    </>
+      <button
+        onClick={() => setWatchlistMarket(!watchlistMarked)}
+        className={`${
+          watchlistMarked ? "bg-green-600" : "bg-blue-700"
+        } text-neutral-100 text-sm p-2 rounded-b w-full`}>
+        Watchlist +
+      </button>
+    </div>
   );
 }
